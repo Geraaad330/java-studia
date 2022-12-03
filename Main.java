34665d25baf4afe34892d1b10899c09f29bbaf3b
Geraaad330 <97625881+Geraaad330@users.noreply.github.com>
2022-12-03 19:40:43 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale locale = new Locale ("pl", "PL");
+        System.out.println("Rafał Śmiały");
+    }
+}
